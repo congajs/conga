061c5df5e06afaf9d47da09a8c006658af675911
lampjunkie <marc@marcroulias.com>
2013-05-11 21:39:38 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# conga [![Build Status](https://secure.travis-ci.org/congajs/conga.png)](http://travis-ci.org/congajs/conga)
+
+## Overview
+
+
+
+## Installation
+
+    > npm install conga -g
+
---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the conga module.
+ *
+ * (c) Marc Roulias <marc@lampjunkie.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// nothing yet
